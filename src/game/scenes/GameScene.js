560f770f85e496da52d55af418c4fcf9e2ef59f8
PiGuy141 <prasadbahekar108@gmail.com>
2026-05-07 14:05:18 +0530

    Add the Tank Body

--- src/game/scenes/GameScene.js
+++ src/game/scenes/GameScene.js
@@ -7,21 +7,31 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
+    // Initialize
     state.game.started = true;
     const cellSize = 24;
     const worldSize = 400 * cellSize;
 
-    this.player = this.add.rectangle(400, 300, 50, 50, 0x00ff00);
-    this.physics.add.existing(this.player);
-
-    this.player.body.setDrag(100, 100);
-    this.player.body.setMaxVelocity(260, 260);
-    this.player.setPosition(worldSize / 2, worldSize / 2);
-
     this.velX = 0;
     this.velY = 0;
-    this.maxVelocity = 200;
+    this.maxVelocity = 192;
 
+    // Player Shapes
+    this.pBody = this.add.circle(0, 0, 32, 0x15b5df);
+    this.pBody.setStrokeStyle(4, 0x0f88a9);
+
+    this.weapon = this.add.rectangle(32, 0, 64, 32, 0x9d9d9d);
+    this.weapon.setStrokeStyle(4, 0x787878);
+    this.weapons = this.add.container(0, 0, [this.weapon]);
+
+    this.player = this.add.container(400, 300, [this.weapons, this.pBody]);
+
+    this.physics.add.existing(this.player);
+    this.player.body.setDrag(100, 100);
+    this.player.body.setMaxVelocity(192, 192);
+    this.player.setPosition(worldSize / 2, worldSize / 2);
+
+    // Controls
     this.cursors = this.input.keyboard.createCursorKeys();
     this.keys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
@@ -30,32 +40,30 @@ export default class GameScene extends Phaser.Scene {
       right: Phaser.Input.Keyboard.KeyCodes.D,
     });
 
+    // Map
     this.cameras.main.startFollow(this.player, true, 0.15, 0.15);
     this.physics.world.setBounds(0, 0, worldSize, worldSize);
     this.cameras.main.setBounds(0, 0, worldSize, worldSize);
     this.grid = this.add
-      .grid(
-        0,
-        0,
-        worldSize,
-        worldSize,
-        32,
-        32,
-        0xdddddd,
-        1, // fill (transparent)
-        0xbbbbbb,
-        1, // grid line color (green)
-      )
+      .grid(0, 0, worldSize, worldSize, 32, 32, 0xdddddd, 1, 0xbbbbbb, 1)
       .setOrigin(0, 0);
 
     this.grid.setDepth(0);
     this.player.setDepth(1);
+
+    this.mouseX = 0;
+    this.mouseY = 0;
+
+    window.addEventListener("mousemove", (e) => {
+      this.mouseX = e.clientX;
+      this.mouseY = e.clientY;
+    });
   }
 
   update() {
     // Variables
     const body = this.player.body;
-    const accel = 8;
+    const accel = 6;
     const friction = 0.95;
     let inputX = 0;
     let inputY = 0;
@@ -83,5 +91,17 @@ export default class GameScene extends Phaser.Scene {
 
     body.velocity.x = this.velX;
     body.velocity.y = this.velY;
+
+    const worldPoint = this.cameras.main.getWorldPoint(
+      this.mouseX,
+      this.mouseY,
+    );
+
+    this.weapons.rotation = Phaser.Math.Angle.Between(
+      this.player.x,
+      this.player.y,
+      worldPoint.x,
+      worldPoint.y,
+    );
   }
 }
